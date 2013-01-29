97eb4dfff263a6c8a2276de36a2fa084ebb685d0
Maxime ALAY-EDDINE <maxime.alayeddine@gmail.com>
2013-01-29 23:35:52 +0100

    Updating github api with firstnames

--- js/ei3info.js
+++ js/ei3info.js
@@ -64,22 +64,28 @@ $(document).ready(function() {
     // ------------------------------------
     (function fetchUsersFromGitHub() {
         var alumniContent = "";
+        var usersUrl = [];
         $.getJSON('https://api.github.com/orgs/ecn-info/members', function(datas) {
             for(var i=0;i<datas.length;i++) {
-                var githubPage;
-                $.getJSON(datas[i].url, function(datasUser) {
-                    githubPage = datasUser.url;
-                })
-                .success(function(datasUser) {githubPage = datasUser.url;});
-                console.log(githubPage);
                 alumniContent += '<div class="span4">';
-                alumniContent += '<img src="'+datas[i].avatar_url+'" width="100" alt="Avatar of '+datas[i].login+'"/>';
-                alumniContent += '<h4><a target="_blank" href="'+githubPage+'">'+datas[i].login+'</a></h4>';
+                alumniContent += '<img src="'+datas[i].avatar_url+'" class="img-circle" width="100" alt="Avatar of '+datas[i].login+'"/>';
+                alumniContent += '<h4><a target="_blank" href="https://github.com/'+datas[i].login+'">'+datas[i].login+'</a></h4>';
+                alumniContent += '<div class="content '+datas[i].login+'"></div>';
                 alumniContent += '</div>';
-                alumniContent += '</div>';
+                if(i%3==2) { alumniContent += '<div class="span12"><hr /></div>';}
+                usersUrl.push(datas[i].url);
             }
         })
-        .success(function() {$("#alumni .row").html(alumniContent)});
+        .success(function() {
+            $("#alumni .row").html(alumniContent);
+            for(var i=0;i<usersUrl.length;i++) {
+                $.getJSON(usersUrl[i]).success(function(datas) {
+                    var alumniDetails = "";
+                    if (typeof datas.name !== undefined) { alumniDetails += "<p>"+datas.name+"</p>"; }
+                    $("#alumni .row .span4 ."+datas.login+"").append(alumniDetails);
+                });
+            }
+        });
     })()
     // End of GitHub API
 });
